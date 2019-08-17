70ef69b41b8fafc04b957092f28e6b044b8475ba
leon <gyoomi0709@foxmail.com>
2019-08-18 01:19:28 +0800

    feat(rabbit): manual ack and confirm

--- miracle-service-order/src/main/java/cn/miracle/service/order/config/RabbitmqConfig.java
+++ miracle-service-order/src/main/java/cn/miracle/service/order/config/RabbitmqConfig.java
@@ -1,9 +1,14 @@
 package cn.miracle.service.order.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.CustomExchange;
-import org.springframework.amqp.core.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +22,62 @@ import java.util.HashMap;
  */
 @Configuration
 public class RabbitmqConfig {
+
+    @Autowired
+    private RabbitProperties rabbitProperties;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setAddresses(rabbitProperties.getAddresses());
+        factory.setUsername(rabbitProperties.getUsername());
+        factory.setPassword(rabbitProperties.getPassword());
+        factory.setPublisherConfirms(true);
+        factory.setPublisherReturns(true);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        // 必须设置为 true，不然当 发送到交换器成功，但是没有匹配的队列，不会触发 ReturnCallback 回调
+        // 而且 ReturnCallback 比 ConfirmCallback 先回调，意思就是 ReturnCallback 执行完了才会执行 ConfirmCallback
+        template.setMandatory(rabbitProperties.getTemplate().getMandatory());
+        template.setConfirmCallback(new ConfirmCallbackListener());
+        template.setReturnCallback(new ReturnCallbackListener());
+        return template;
+    }
+
+
+    public static class ConfirmCallbackListener implements RabbitTemplate.ConfirmCallback {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(ConfirmCallbackListener.class);
+
+        @Override
+        public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+            LOGGER.info("----------------start-------------------");
+            LOGGER.info("CorrelationData -> {}", correlationData);
+            LOGGER.info("ack -> {}", ack);
+            LOGGER.info("cause -> {}", cause);
+            LOGGER.info("----------------end-------------------");
+        }
+    }
+
+    public static class ReturnCallbackListener implements RabbitTemplate.ReturnCallback {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(ReturnCallbackListener.class);
+
+        @Override
+        public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+            LOGGER.info("----------------start-------------------");
+            LOGGER.info("Message -> {}", message);
+            LOGGER.info("replyCode -> {}", replyCode);
+            LOGGER.info("replyText -> {}", replyText);
+            LOGGER.info("exchange -> {}", exchange);
+            LOGGER.info("routingKey -> {}", routingKey);
+            LOGGER.info("----------------end-------------------");
+        }
+    }
 
     /**
      * the exchange of delay
@@ -51,4 +112,6 @@ public class RabbitmqConfig {
     public Binding delayBinding() {
         return BindingBuilder.bind(delayQueue()).to(delayExchange()).with("delay_key").noargs();
     }
+
+
 }
--- miracle-service-order/src/main/java/cn/miracle/service/order/mq/OrderListener.java
+++ miracle-service-order/src/main/java/cn/miracle/service/order/mq/OrderListener.java
@@ -1,9 +1,12 @@
 package cn.miracle.service.order.mq;
 
 import cn.miracle.framework.model.order.Order;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -17,10 +20,16 @@ import java.util.Objects;
 public class OrderListener {
 
     @RabbitListener(queues = "delay_queue")
-    public void listenOrderCancel(Order order) {
+    public void listenOrderCancel(Channel channel, Message message, Order order) {
         if (Objects.equals(10, order.getOrderStatus())) {
             // 设置订单为超时过期，并存入数据库
             System.out.println(LocalDateTime.now() + "  订单编号: " + order.getOrderNo() + " 已超时支付，已自动取消！");
+        }
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        try {
+            channel.basicAck(deliveryTag, true);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

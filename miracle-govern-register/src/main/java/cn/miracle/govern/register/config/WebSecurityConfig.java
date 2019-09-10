5bf29e2a853cd9061ead9ca03e12f8fdca8a3197
leon <gyoomi0709@foxmail.com>
2019-09-10 22:40:39 +0800

    feat(http): http basic of eureka

--- miracle-govern-register/src/main/java/cn/miracle/govern/register/config/WebSecurityConfig.java
+++ miracle-govern-register/src/main/java/cn/miracle/govern/register/config/WebSecurityConfig.java
@@ -1,29 +1,29 @@
-///**
-// * Copyright © 2019, LeonKeh
-// * <p>
-// * All Rights Reserved.
-// */
-//
-//package cn.miracle.govern.register.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-///**
-// * 类功能描述
-// *
-// * @author Leon
-// * @version 2019/9/10 10:25
-// */
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated().and().httpBasic();
-//    }
-//}
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package cn.miracle.govern.register.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/9/10 10:25
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated().and().httpBasic();
+    }
+}
